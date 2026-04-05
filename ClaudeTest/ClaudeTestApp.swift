89625989c2bfc0678d9d58dcc3fef843cfdfcb3d
Malik Abdul Ghani <malik.ghani@astronauts.id>
2026-04-06 02:24:24 +0700

    Initial Commit

---
+++ ClaudeTest/ClaudeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeTestApp.swift
+//  ClaudeTest
+//
+//  Created by Malik Abdul Ghani on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
